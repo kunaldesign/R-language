fd2f37abffea17e69d8a55d4ee23c6415e055429
kunaldesign <kunalhedaoo25@gmail.com>
2020-02-27 21:27:22 +0530

    updated now

--- program 1.r
+++ program 1.r
@@ -1,4 +1,8 @@
 '''program to print string'''
+
+#We can use the print() function
 print("hello world")
+#Quote can be suppressed in the output
 print("hello world",quote=FALSE)
+#if there are more than 1 item, we cen concatenate using paste()
 print(paste("how","are","you"))
---
+++ program 3.r
@@ -0,0 +1,25 @@
+ #Program to check if the input number is prime or not
+
+#take in input from user
+num=as.integer(readline(prompt="enter a number"))
+
+flag=0
+
+#prime numbers are greater than 1 
+if (num>1) {
+   #check for the factors
+   flag=1
+   for (i in 2:(num-1)) {
+   if ((num %% i)==0) {
+      flag=0
+      break
+   }
+}
+}
+if(num==2)
+    flag=1
+if (flag==1) {
+    print(paste(num,"is a prime  number"))
+} else {
+   print(paste(num,"is not a prime number"))
+}
